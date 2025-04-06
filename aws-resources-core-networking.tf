454f63d54093fae1218871c56e6ef6691e0cb781
MarSegu <mariosegura0904@gmail.com>
2025-04-05 19:56:49 -0500

    new file:   aws-resources-core-networking.tf

---
+++ aws-resources-core-networking.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "cloudlake_core" {
+  cidr_block = "10.0.0.0/16"
+  tags = var.tags
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "private_az1" {
+  vpc_id                  = aws_vpc.cloudlake_core.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = false
+  tags = var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+# Environment setting
+variable "environment" {
+  description = "The environment in which the resources are being deployed (e.g., dev, prod)."
+  default = "dev"
+}
+
+# Tags for resource organization and identification
+variable "tags" {
+  description = "Tags to assign to the resources for categorization and tracking."
+  type        = map(string)
+  default     = {
+    environment = "dev"
+    application = "cloudlake"
+    terraform   = "true"
+  }
+}
+
+variable "project_name" {
+  type    = string
+  default = "cloudlake"
+}
