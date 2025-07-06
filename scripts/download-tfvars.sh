daabbe13a07bb1b7759bd3781d828be1e8413462
MarSegu <mariosegura0904@gmail.com>
2025-07-06 18:50:41 -0500

    modified:   scripts/download-tfvars.sh

--- scripts/download-tfvars.sh
+++ scripts/download-tfvars.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+set -e
 
-# install AWS CLI (if not already in image)
-apk add --no-cache curl unzip python3 py3-pip
-pip install awscli
+if ! command -v aws >/dev/null 2>&1; then
+  echo "Installing AWS CLI..."
+  apk add --no-cache curl unzip python3 py3-pip
+  pip install awscli
+fi
 
-# download tfvars file from S3
+echo "Downloading terraform.tfvars from S3..."
 aws s3 cp s3://cloudlake-directory-tf-vars/envs/dev/terraform.tfvars terraform.tfvars
